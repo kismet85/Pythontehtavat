008dcd22148fca96c2539e31d26ad165b9de8a08
kismet <kismet1985@hotmail.fi>
2023-08-19 12:31:56 +0300

    Tehtävä 2-3

---
+++ Harjoitustehtävät2-3.py
@@ -0,0 +1,8 @@
+kanta = int(input("Anna suorakulmion kanta. \n"))
+korkeus = int(input("Anna suorakulmion korkeus. \n"))
+
+piiri = (kanta * 2) + (korkeus * 2)
+pinta = kanta * korkeus
+
+print(f"Suorakulmion piiri on {piiri}")
+print(f"Suorakulmion pinta-ala on {pinta}")
