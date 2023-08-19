7b07f1fc05dec60ce49cab005f1ce84f3b735e6b
kismet <kismet1985@hotmail.fi>
2023-08-19 12:38:08 +0300

    4

---
+++ Harjoitustehtävät2-4.py
@@ -0,0 +1,11 @@
+luku1 = int(input("Anna ensimmäinen kokonaisluku: \n"))
+luku2 = int(input("Anna toinen kokonaisluku: \n"))
+luku3 = int(input("Anna kolmannes kokonaisluku: \n"))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print(f"Lukujen summa on {summa}")
+print(f"Lukujen tulo on {tulo}")
+print(f"Lukujen keskiarvo on {keskiarvo}")
