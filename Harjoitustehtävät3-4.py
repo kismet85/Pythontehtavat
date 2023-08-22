dcc21a93b9149f9f498d0e21670dd4c10e058b90
Ismet Ymeri <kismet1985@hotmail.fi>
2023-08-22 19:02:29 +0300

    Delete Harjoitustehtävät3-4.py

--- Harjoitustehtävät3-4.py
+++
@@ -1,8 +0,0 @@
-vuosi = int(input("Syötä vuosi: \n"))
-
-if (vuosi % 400 == 0) and (vuosi % 100 == 0):
-    print(f"vuosi {vuosi} on karkausvuosi.")
-elif (vuosi % 4 == 0) and (vuosi % 100 != 0):
-    print(f"vuosi {vuosi} on karkausvuosi.")
-else:
-    print(f"vuosi 0{vuosi} ei ole karkausvuosi.")
