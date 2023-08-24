5742f2b937437b59059b06441e0fb43d75b5ba9d
kismet <kismet1985@hotmail.fi>
2023-08-24 14:19:58 +0300

    5-2

---
+++ Moduuli 5/Harjoitustehtävät5-2.py
@@ -0,0 +1,12 @@
+numerot = []
+
+
+numba = input("Syötä luku: \n")
+while numba!="":
+    num1 = float(numba)
+    numerot.append(num1)
+    numerot.sort()
+    numba = input("Syötä luku: \n")
+
+maxList = sorted(numerot, reverse=True)
+print(maxList[:5])
