d0c7e12d0cb188b2dd1bec34fe1076e8f920e389
kismet <kismet1985@hotmail.fi>
2023-08-28 09:21:23 +0300

    6-2

---
+++ Moduuli 6/Harjoitustehtävät6-3.py
@@ -0,0 +1,17 @@
+# Kirjoita funktio, joka saa parametrinaan bensiinin määrän Yhdysvaltain nestegallonoina ja palauttaa paluuarvonaan vastaavan litramäärän.
+# Kirjoita pääohjelma, joka kysyy gallonamäärän käyttäjältä ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen.
+# Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän. Yksi gallona on 3,785 litraa.
+
+def laskeGallonat(gallonat):
+    litra = gallonat*3.785
+    print(litra)
+    return
+gallonaMaara = 1
+
+while True:
+    gallonaMaara = float((input("Syötä gallonamäärä: \n")))
+    if gallonaMaara>0:
+        laskeGallonat(gallonaMaara)
+    else:
+        break
+print("Syötit pienemmän määrän kuin 0.")
