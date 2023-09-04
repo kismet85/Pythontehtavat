ead7d394950b669165d59e947a8f81a6f79e3834
kismet <kismet1985@hotmail.fi>
2023-09-04 11:10:47 +0300

    6-1

--- Moduuli 6/Harjoitustehtävät6-1.py
+++ Moduuli 6/Harjoitustehtävät6-1.py
@@ -1,11 +1,13 @@
 # Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6. Kirjoita pääohjelma,
 # joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
 import random
+def noppa():
+    return random.randint(1, 6)
 
-nopanhaku = 6
+def main():
+    noppa1 = 0
+    while noppa1 != 6:
+        noppa1 = noppa()
+        print(f"Nopanarvo on {noppa1}")
 
-while True:
-    nopanarvo = random.randint(1,6)
-    print(f"Nopanarvo on {nopanarvo}")
-    if(nopanarvo==6):
-        break
+main()
