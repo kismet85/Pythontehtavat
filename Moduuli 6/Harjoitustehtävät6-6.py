e9b434af003b2d262685ed9f889997a4c1a9c0ec
kismet <kismet1985@hotmail.fi>
2023-09-05 11:28:32 +0300

    6-6

--- Moduuli 6/Harjoitustehtävät6-6.py
+++ Moduuli 6/Harjoitustehtävät6-6.py
@@ -4,19 +4,28 @@
 # Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
 
 import math
+
+
 def main():
+    kutsuttu = 1
     while True:
         pizzaHalkaisija1 = input("Syötä ensimmäisen pizzan halkaisija: \n")
         pizzaHalkaisija2 = input("Syötä toisen pizzan halkaisija: \n")
         pizzaHinta1 = input("Syötä ensimmäisen pizzan hinta: \n")
         pizzaHinta2 = input("Syötä toisen pizzan hinta: \n")
         if pizzaHalkaisija1 !="" and pizzaHalkaisija2 !="" and pizzaHinta1 !="" and pizzaHinta2 !="":
-            pizza1 = yksikkoHinta(float(pizzaHalkaisija1), float(pizzaHinta1))
-            pizza2 = yksikkoHinta(float(pizzaHalkaisija2), float(pizzaHinta2))
-def yksikkoHinta(halkaisija, hinta):
+            pizza1 = yksikkoHinta(float(pizzaHalkaisija1), float(pizzaHinta1), kutsuttu)
+            kutsuttu +=1
+            pizza2 = yksikkoHinta(float(pizzaHalkaisija2), float(pizzaHinta2), kutsuttu)
+
+def yksikkoHinta(halkaisija, hinta,kutsuttu):
+
     area = (math.pi * (((halkaisija/100) / 2) ** 2))
     yksikkohinta=hinta/area
-    print(float(round(yksikkohinta, 2)))
+    hinta = float(round(yksikkohinta, 2))
+    print(f"Pizza {kutsuttu} hinta on {hinta}")
+
+
 
 main()
 
